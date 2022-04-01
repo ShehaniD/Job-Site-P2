78dab9275c7c371a2e527c180cbf43e0cc431b73
Daniel <edanielarredondo@gmail.com>
2022-04-01 17:47:05 -0500

    finish template variable parameters for display.html

--- P2_Starter_Code/app.py
+++ P2_Starter_Code/app.py
@@ -37,37 +37,38 @@ def upload():
 
 @app.route("/display")
 def display():
-    # pdfFileObj = open('static/sample_resume.pdf', 'rb')
-    # # Creating a pdf reader object
-    # pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    # # Getting number of pages in pdf file
-    # pages = pdfReader.numPages
-    # # Loop for reading all the Pages
-    # for i in range(pages):
-    #     # Creating a page object
-    #     pageObj = pdfReader.getPage(i)
-    #     # Printing Page Number
-    #     print("Page No: ", i)
-    #     # Extracting text from page
-    #     # And splitting it into chunks of lines
-    #     text = pageObj.extractText().split('\n')
-    #     # Finally the lines are stored into list
-    #     # For iterating over list a loop is used
-    #     user['name'] = text[5]
-    #     user['email'] = text[18]
+    pdfFileObj = open('static/sample_resume.pdf', 'rb')
+    # Creating a pdf reader object
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    # Getting number of pages in pdf file
+    pages = pdfReader.numPages
+    # Loop for reading all the Pages
+    for i in range(pages):
+        # Creating a page object
+        pageObj = pdfReader.getPage(i)
+        # Printing Page Number
+        print("Page No: ", i)
+        # Extracting text from page
+        # And splitting it into chunks of lines
+        text = pageObj.extractText().split('\n')
+        # Finally the lines are stored into list
+        # For iterating over list a loop is used
+        user['name'] = text[5]
+        user['email'] = text[18]
 
-    #     print(user['name'])
-    #     print(user['email'])
+        print(user['name'])
+        print(user['email'])
 
 
-    #     # for i in range(len(text)):
-    #     #     # Printing the line
-    #     #     # Lines are seprated using "\n"
-    #     #     print(text[i], end="\n")
-    #     #     # For Seprating the Pages
+        # for i in range(len(text)):
+        #     # Printing the line
+        #     # Lines are seprated using "\n"
+        #     print(text[i], end="\n")
+        #     # For Seprating the Pages
         
-    # # closing the pdf file object
-    # pdfFileObj.close()
+    # closing the pdf file object
+    pdfFileObj.close()
+    
     return render_template('display.html', first_name="Steve", last_name="Jobs", address="123 Apple St.")
 
 
