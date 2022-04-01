aba8cdc6e1f31d43e29c1e3fb3d7eb658474e812
Daniel <edanielarredondo@gmail.com>
2022-04-01 17:42:40 -0500

    Update display.html with jinja variables

--- P2_Starter_Code/app.py
+++ P2_Starter_Code/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
+from commonregex import CommonRegex
 import PyPDF2
 
 from werkzeug.utils import secure_filename
@@ -19,35 +20,7 @@ app.config['DROPZONE_REDIRECT_VIEW'] = 'display'
 
 @app.route('/')
 def index():  # put application's code here
-    pdfFileObj = open('uploads/sampleresume.pdf', 'rb')
-    # Creating a pdf reader object
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    # Getting number of pages in pdf file
-    pages = pdfReader.numPages
-    # Loop for reading all the Pages
-    for i in range(pages):
-        # Creating a page object
-        pageObj = pdfReader.getPage(i)
-        # Printing Page Number
-        print("Page No: ", i)
-        # Extracting text from page
-        # And splitting it into chunks of lines
-        text = pageObj.extractText().split('\n')
-        # Finally the lines are stored into list
-        # For iterating over list a loop is used
-        user['name'] = text[5]
-        user['email'] = text[18]
-
-        for i in range(len(text)):
-            # Printing the line
-            # Lines are seprated using "\n"
-            print(text[i], end="\n")
-            # For Seprating the Pages
-        
-    # closing the pdf file object
-    pdfFileObj.close()
-    return render_template("index.html", user=user)
-
+    return render_template("index.html")
 
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
@@ -59,11 +32,43 @@ def upload():
             if file_ext not in app.config['UPLOAD_EXTENSIONS']:
                 abort(400)
             uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+            return redirect('display')
     return render_template("upload.html")
 
 @app.route("/display")
 def display():
-    return render_template()
+    # pdfFileObj = open('static/sample_resume.pdf', 'rb')
+    # # Creating a pdf reader object
+    # pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    # # Getting number of pages in pdf file
+    # pages = pdfReader.numPages
+    # # Loop for reading all the Pages
+    # for i in range(pages):
+    #     # Creating a page object
+    #     pageObj = pdfReader.getPage(i)
+    #     # Printing Page Number
+    #     print("Page No: ", i)
+    #     # Extracting text from page
+    #     # And splitting it into chunks of lines
+    #     text = pageObj.extractText().split('\n')
+    #     # Finally the lines are stored into list
+    #     # For iterating over list a loop is used
+    #     user['name'] = text[5]
+    #     user['email'] = text[18]
+
+    #     print(user['name'])
+    #     print(user['email'])
+
+
+    #     # for i in range(len(text)):
+    #     #     # Printing the line
+    #     #     # Lines are seprated using "\n"
+    #     #     print(text[i], end="\n")
+    #     #     # For Seprating the Pages
+        
+    # # closing the pdf file object
+    # pdfFileObj.close()
+    return render_template('display.html', first_name="Steve", last_name="Jobs", address="123 Apple St.")
 
 
 if __name__ == '__main__':
