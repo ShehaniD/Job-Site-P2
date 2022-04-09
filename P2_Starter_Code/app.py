4a95b8ee8ad77612c283aa2f518965f0c4511a1c
Kwen4758 <kdog@wentland.net>
2022-04-08 21:06:39 -0500

    feat: fill in almost all forms

--- P2_Starter_Code/app.py
+++ P2_Starter_Code/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
-from commonregex import CommonRegex
 import PyPDF2
+import re
 
 from werkzeug.utils import secure_filename
 import os
@@ -45,22 +45,24 @@ def display(filename: str):
 
     rawText = ''
     # Open file
-    with open(abs_file_path, mode='rb') as f:
-        reader = PyPDF2.PdfFileReader(f)
+    with open(abs_file_path, mode='rb') as file:
+        reader = PyPDF2.PdfFileReader(file)
         for i in range(reader.numPages):
             page = reader.getPage(i)
             rawText += page.extractText()
 
-    parsed_text = CommonRegex(rawText)
-
-    # Trying CommonRegex out. These will parse the text of pdf and
-    # return an array with any values associated with respective parsed_text.attribute
-    # Documentation - https://github.com/madisonmay/CommonRegex
-    address = parsed_text.street_addresses[0]
-    email = parsed_text.emails[0]
+    emails = re.findall('''(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])''', rawText)
+    zipcodes = re.findall('''^\d{5}-\d{4}|\d{5}|[A-Z]\d[A-Z] \d[A-Z]\d$''', rawText)
+    streets = re.findall('''\d+[ ](?:[A-Za-z0-9.-]+[ ]?)+(?:Avenue|Lane|Road|Boulevard|Drive|Street|Ave|Dr|Rd|Blvd|Ln|St)\.?''', rawText)
+    states = re.findall('''(Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New[ ]Hampshire|New[ ]Jersey|New[ ]Mexico|New[ ]York|North[ ]Carolina|North[ ]Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode[ ]Island|South[ ]Carolina|South[ ]Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West[ ]Virginia|Wisconsin|Wyoming)''', rawText)
+    
+    email = emails[0] if len(emails) > 0 else ''
+    zipcode = zipcodes[0] if len(zipcodes) > 0 else ''
+    street = streets[0] if len(streets) > 0 else ''
+    state = states[0] if len(states) > 0 else ''
 
     # Render Template and pass in values for form fields if present
-    return render_template('display.html', first_name='Hi', last_name='there', address=address, email=email)
+    return render_template('display.html', email=email, zipcode=zipcode, address=street, state=state)
 
 
 if __name__ == '__main__':
